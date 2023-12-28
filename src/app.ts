015561f47ce35ca592d656bd81424ef3706984f9
krishi-nagar <kinjal.nagar.23@gmail.com>
2023-12-28 21:30:23 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
-import express from 'express';
-//npm i -D @types/express
-// npm i -D ts-node nodemon
+import express,{Request,Response,NextFunction} from 'express';
+import logger from './config/logger';
+import { HttpError } from 'http-errors';
+
+
+
 const app = express();
 
 app.get('/',(req,res)=>{
+
    res.send("Welcome to Auth Service");
 })
+app.use((err : HttpError, req :Request, res :Response, next : NextFunction)=>{
+   
+   logger.error(err.message); 
+   const statusCode =  err.statusCode || 500;
+   res.status(statusCode).json({
+      errors : [
+         {
+            type : err.name,
+            msg : err.message,
+            path : '',
+            location : '',
+         }
+      ]
+   });
 
+   
+})
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import app from "./app";
 import logger from "./config/logger";
 
 
+
 const startServer =  () => {
     const PORT = Config.PORT
  
     try {
+        
         app.listen(PORT,()=> logger.info(`Listening on port ${PORT}`));
     } catch (err : unknown) {
         if (err instanceof Error) {
