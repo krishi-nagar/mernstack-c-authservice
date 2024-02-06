2acc487726e62ddf2b8bef218d91db16b478080c
krishi-nagar <kinjal.nagar.23@gmail.com>
2024-02-06 20:42:24 +0530

    add test for persist user

---
+++ src/services/UserService.ts
@@ -0,0 +1,14 @@
+import { AppDataSource } from "../config/data-source";
+import { User } from "../entity/User";
+import { UserData } from "../types";
+import { Repository } from "typeorm";
+export class UserService {
+    
+    // constructor using for the dependency injection process
+    constructor(private userRepository: Repository<User>){}
+    
+    async create({firstName, lastName,email, password}:UserData){
+        await this.userRepository.save({ firstName, lastName,email, password })
+    }
+
+}
--- src/tests/users/register.spec.ts
+++ src/tests/users/register.spec.ts
@@ -1,16 +1,34 @@
 import request from 'supertest';
 import app from "./../../app";
+import { User } from '../../entity/User';
+import { DataSource } from 'typeorm';
+import { AppDataSource } from '../../config/data-source';
+import { truncateTables } from '../utils';
 
 describe("POST/auth/register",()=>{
 
+    let connection : DataSource;
+
+    beforeAll(async()=>{
+        connection = await AppDataSource.initialize();
+    })
+    
+    beforeEach(async()=>{
+        //Database truncate
+        await truncateTables(connection);
+    })
+
+    afterAll(async()=>{
+        await connection.destroy();
+    })
     describe("Given All Fields" ,()=>{
 
         it("should return the 201 status code",async()=>{
             //AAA => Arange ,Act ,Assert
             // Arange
             const userData = {
-                firstname : "Kinjal",
-                lastname : "Nagar",
+                firstName : "Kinjal",
+                lastName : "Nagar",
                 email : "knagar@gmail.com",
                 password : "secret",
             };
@@ -27,8 +45,8 @@ describe("POST/auth/register",()=>{
         it("should return valid JSON response",async()=>{
             
             const userData = {
-                firstname : "Kinjal",
-                lastname : "Nagar",
+                firstName : "Kinjal",
+                lastName : "Nagar",
                 email : "knagar@gmail.com",
                 password : "secret",
             };
@@ -43,8 +61,8 @@ describe("POST/auth/register",()=>{
         it("should persist the user in the database",async()=>{
 
             const userData = {
-                firstname : "Kinjal",
-                lastname : "Nagar",
+                firstName : "Kinjal",
+                lastName : "Nagar",
                 email : "knagar@gmail.com",
                 password : "secret",
             };
@@ -52,6 +70,15 @@ describe("POST/auth/register",()=>{
              await request(app)
                 .post("/auth/register")
                 .send(userData);
+
+                //Assert
+                const userRepository = connection.getRepository(User)
+                const users = await userRepository.find();
+                expect(users).toHaveLength(1);
+                expect(users[0].firstName).toBe(userData.firstName);
+                expect(users[0].lastName).toBe(userData.lastName);
+                expect(users[0].email).toBe(userData.email);
+                expect(users[0].password).toBe(userData.password);
 
         })
     })
