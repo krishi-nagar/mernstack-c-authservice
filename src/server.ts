251d7ffca1eb087c8d01b53fc744bbbaafe2b3d0
krishi-nagar <kinjal.nagar.23@gmail.com>
2024-03-19 21:04:47 +0530

    add dbconnection to server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import {Config } from "./config";
 import app from "./app";
 import logger from "./config/logger";
+import { AppDataSource } from "./config/data-source";
 
 // docker test
 
-const startServer =  () => {
+const startServer =  async() => {
     const PORT = Config.PORT
  
     try {
-        
+        await AppDataSource.initialize();
+        logger.info("Hey Krishi, Database Connected Successfully !!")
         app.listen(PORT,()=> logger.info(`Listening on port ${PORT}`));
     } catch (err : unknown) {
         if (err instanceof Error) {
