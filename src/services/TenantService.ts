2e4f41ad74d4ff27b4a5d7f1f05eadda0744cad0
krishi-nagar <kinjal.nagar.23@gmail.com>
2024-04-12 20:25:47 +0530

    Tenant Management Api

--- src/services/TenantService.ts
+++ src/services/TenantService.ts
@@ -2,11 +2,26 @@ import { Repository } from "typeorm";
 import { ITenant } from "../types";
 import { Tenant } from "../entity/Tenant";
 
-export class TenantService{
-    constructor(private tenantrepository:Repository<Tenant>){
+export class TenantService {
+  constructor(private tenantRepository: Repository<Tenant>) {}
 
-    }
-    async create(tenantData:ITenant){
-        return await this.tenantrepository.save(tenantData)
-    }
+  async create(tenantData: ITenant) {
+    return await this.tenantRepository.save(tenantData);
+  }
+
+  async update(id: number, tenantData: ITenant) {
+    return await this.tenantRepository.update(id, tenantData);
+  }
+
+  async getAll() {
+    return await this.tenantRepository.find();
+  }
+
+  async getById(tenantId: number) {
+    return await this.tenantRepository.findOne({ where: { id: tenantId } });
+  }
+
+  async deleteById(tenantId: number) {
+    return await this.tenantRepository.delete(tenantId);
+  }
 }
