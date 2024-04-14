b146a95bbc348f7c831ae97d98154e22bcaf602a
krishi-nagar <kinjal.nagar.23@gmail.com>
2024-04-14 18:57:41 +0530

    User Management Api Assignment

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,12 +7,13 @@ import { UserService } from "../services/UserService";
 import { AppDataSource } from "../config/data-source";
 import { User } from "../entity/User";
 import { CreateUserRequest } from "../types";
+import logger from "../config/logger";
 
 const router = express.Router();
 
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const userController = new UserController(userService);
+const userController = new UserController(userService, logger);
 
 router.post(
   "/",
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,7 +1,7 @@
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
 import { User } from "../entity/User";
-import { UserData } from "../types";
+import { LimitedUserData, UserData } from "../types";
 import { Repository } from "typeorm";
 
 export class UserService {
@@ -38,5 +38,28 @@ export class UserService {
   }
   async findById(id: number) {
     return await this.userRepository.findOne({ where: { id } });
+  }
+  async update(userId: number, { firstName, lastName, role }: LimitedUserData) {
+    try {
+      return await this.userRepository.update(userId, {
+        firstName,
+        lastName,
+        role,
+      });
+    } catch (err) {
+      const error = createHttpError(
+        500,
+        "Failed to update the user in the database",
+      );
+      throw error;
+    }
+  }
+
+  async getAll() {
+    return await this.userRepository.find();
+  }
+
+  async deleteById(userId: number) {
+    return await this.userRepository.delete(userId);
   }
 }
